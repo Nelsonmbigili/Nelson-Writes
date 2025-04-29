079ac353cac276e4a4fde6f8ee3723a061028805
Nelson Mbigili <107414047+Nelsonmbigili@users.noreply.github.com>
2025-04-29 14:57:38 +0200

    Adding Google meta Link

--- src/components/seo.js
+++ src/components/seo.js
@@ -37,6 +37,7 @@ function Seo({ description, title, children }) {
       <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
+      <meta name="google-site-verification" content="HS8rYetmahVib0ImdQYcUXfXSKSmQI8KdiYZ9P9AzBg" />
       {children}
     </>
   )
